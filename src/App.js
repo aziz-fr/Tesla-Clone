a61b7e0b5395351c1e32be406f9dd700256de48d
Aziz <aziz.tech80@gmail.com>
2024-08-21 15:39:30 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sections from "./components/Sections";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="main">
+      <Navbar />
+      <Sections />
+    </div>
+  );
+}
+
+export default App;
